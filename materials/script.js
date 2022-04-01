e4c3a1d7020c87f20bcc82a5ea98f5c23245c996
CarsonBurke <48334001+CarsonBurke@users.noreply.github.com>
2022-04-01 14:59:43 -0700

    Initial commit

---
+++ materials/script.js
@@ -0,0 +1,56 @@
+// Slide logic
+
+const slides = document.getElementsByClassName('slideParent')
+
+let slideIndex = 0,
+    scrollTop = 0
+
+document.addEventListener('keydown', hotKeyManager)
+
+function hotKeyManager(event) {
+
+    const key = event.key
+
+    if (key == 'ArrowLeft' || key == 'a') {
+
+        previousSlide()
+        return
+    }
+    if (key == 'ArrowRight' || key == 'd') {
+
+        nextSlide()
+        return
+    }
+}
+
+function nextSlide() {
+
+    if (slideIndex + 1 == slides.length) return
+
+    slideIndex++
+
+    const slide = slides[slideIndex]
+
+    scrollTop = slide.offsetTop
+    console.log(scrollTop)
+    window.scroll({
+        top: scrollTop,
+        behavior: 'smooth'
+    })
+}
+
+function previousSlide() {
+
+    if (slideIndex - 1 == -1) return
+
+    slideIndex--
+
+    const slide = slides[slideIndex]
+
+    scrollTop = slide.offsetTop
+
+    window.scroll({
+        top: scrollTop,
+        behavior: 'smooth'
+    })
+}
